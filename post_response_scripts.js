313fa88710ab09c5a7e122ca8f94c9d94ac70c26
Kavindu <kmvithanage48@gmail.com>
2024-06-02 22:31:57 +0530

    updates

--- post_response_scripts.js
+++ post_response_scripts.js
@@ -11,3 +11,18 @@ pm.test("Status name is OK", () =>{
 pm.test("Succsfull get request", () =>{
     pm.expect(pm.response.code).to.be.oneOf([200,201]);
 })
+
+
+// Response header validation
+
+pm.test("Header content-type is present", () =>{
+    pm.response.to.have.header("Content-Type");
+})
+
+pm.test("Header content-Type is application/json", () =>{
+    pm.expect(pm.response.headers.get("Content-Type")).to.eql('application/json');
+})
+
+
+// Response cookies validation
+
