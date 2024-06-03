c65d25a77c88fe3b52a45df27c0f350d09aa6409
Kavindu <kmvithanage48@gmail.com>
2024-06-04 00:49:41 +0530

    updates

--- post_response_scripts.js
+++ post_response_scripts.js
@@ -26,3 +26,49 @@ pm.test("Header content-Type is application/json", () =>{
 
 // Response cookies validation
 
+pm.test("Cookie 'language' is present", () => {
+    pm.cookies.has('language');
+})
+
+pm.test("cookies language is eql1", () =>{
+    pm.expect(pm.cookies.get('language')).to.eql('eql1');
+})
+
+ 
+// Response time validation
+pm.test("Response time is less than 50ms", () =>{
+    pm.expect(pm.response.responseTime).to.below(50);
+})
+
+
+// Asserting  or validate response body value type
+const jsonData = pm.response.json()
+
+pm.test("Test data type is correct", () => {
+    pm.expect(jsonData).to.be.an("object");
+    pm.expect(jsonData.user_id).to.be.a("number");
+    pm.expect(jsonData.email).to.be.a("string");
+    pm.expect(jsonData.name).to.be.a("string");
+    pm.expect(jsonData.nicknames).to.be.an("array");
+})
+
+
+// Asserting array properties
+pm.test("Array one properties is correct", () => {
+    pm.expect(jsonData.nicknames).to.include("test");
+    pm.expect(jsonData.nicknames).to.have.include(["test","test 2", "test 3", "test 4"]);
+})
+
+
+// Validate response body value
+pm.test("Test data values is correct", () => {
+    pm.expect(jsonData.user_id).to.eql(1);
+    pm.expect(jsonData.email).to.eql("test@test.com");
+    pm.expect(jsonData.name).to.eql("test@test.com");
+    pm.expect(jsonData.nicknames[0]).to.eql("test");
+    pm.expect(jsonData.email_verified).to.eql("true");
+})
+
+
+
+
